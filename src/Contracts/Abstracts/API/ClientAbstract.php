5b33942c263f0eee69a2804847d9f1a088234de4
DSchuppelius <daniel.schuppelius@gmail.com>
2024-10-25 13:44:03 +0200

    change access to requestWithRetry

--- src/Contracts/Abstracts/API/ClientAbstract.php
+++ src/Contracts/Abstracts/API/ClientAbstract.php
@@ -125,7 +125,7 @@ abstract class ClientAbstract implements ApiClientInterface {
         return $response;
     }
 
-    private function requestWithRetry(string $method, string $uri, array $options = [], int $maxRetries = 3, int $retryDelay = 3): ResponseInterface {
+    protected function requestWithRetry(string $method, string $uri, array $options = [], int $maxRetries = 3, int $retryDelay = 3): ResponseInterface {
         $attempt = 0;
 
         while ($attempt < $maxRetries) {
