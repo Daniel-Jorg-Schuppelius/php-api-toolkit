1c95738809c8294c64f80791643937c89a209c59
DSchuppelius <daniel.schuppelius@gmail.com>
2026-01-11 12:11:25 +0100

    ide update

--- tests/TestEntities/StringCheckers.php
+++ tests/TestEntities/StringCheckers.php
@@ -15,6 +15,9 @@ namespace Tests\TestEntities;
 use APIToolkit\Contracts\Abstracts\NamedValues;
 use Psr\Log\LoggerInterface;
 
+/**
+ * @extends NamedValues<StringChecker>
+ */
 class StringCheckers extends NamedValues {
     public function __construct($data = null, ?LoggerInterface $logger = null) {
         $this->entityName = "content";
